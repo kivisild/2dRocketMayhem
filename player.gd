a8d381af77c10d9dd5cf40129b9505cc128ea153
kivisild <siimmarten.schmidt@gmail.com>
2025-04-29 13:36:44 +0300

    Added coyote-time, made the clouds move

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 const max_jumps = 2
 
@@ -9,37 +9,34 @@ var currentJumps = 0
 var jumping = false
 var was_on_floor = false;
 
-
+@onready var jump_leeway_timer = $JumpLeewayTimer
 @onready var anim = get_node("AnimationPlayer")
 
+
+
 func _physics_process(delta: float) -> void:
-	if is_on_floor():
-		if !was_on_floor:
-			# Landed
-			currentJumps = 0
-			jumping = false
-			
-	else:
-		if was_on_floor:
-			if !jumping:
-				currentJumps = 1
-		
-		
-	
 	was_on_floor = is_on_floor()
+	
+		
+	if (was_on_floor and !is_on_floor()):
+		jump_leeway_timer.start()
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	
+	
 
 	# Handle jump.
-	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and currentJumps < max_jumps:
+	# Coyote time
+	if ((Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and currentJumps < max_jumps) or ((Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("ui_up")) and !jump_leeway_timer.is_stopped() and !jumping):
 		velocity.y = JUMP_VELOCITY
 		jumping = true
 		currentJumps += 1
 		anim.play("Jump")
+		print("Jumping")
 		
-
-	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -62,3 +59,14 @@ func _physics_process(delta: float) -> void:
 		#anim.play("Fall")
 
 	move_and_slide()
+	if (is_on_floor() and !was_on_floor):
+			# Landed
+			currentJumps = 0
+			jumping = false
+			print("Landed - Is on floor and was not on floor")
+			
+		
+
+
+func _on_jump_leeway_timer_timeout() -> void:
+	pass
